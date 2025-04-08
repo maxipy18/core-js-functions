8b1198b2ed0883a0243f0e2e931c2f333ec4e0ca
Maxim Krestyaninov <maxipy@ya.ru>
2025-04-08 21:26:16 +0400

    feat: add memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -116,8 +116,14 @@ function getPolynom(...arg) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+  return function cacheFunc() {
+    if (!cache) {
+      cache = func();
+    }
+    return cache;
+  };
 }
 
 /**
