02a0695c07729f2d183944340b088e8826be99d4
Maxim Krestyaninov <maxipy@ya.ru>
2025-04-08 21:00:22 +0400

    feat: add getArgumentsCount

--- src/test.js
+++ src/test.js
@@ -1,1 +1,14 @@
-console.log('aaa');
+function getArgumentsCount(arr) {
+  const result = arr.map((item) => item.length);
+  console.log(result);
+}
+
+getArgumentsCount([
+  function () {
+    console.log('hello world');
+  },
+  function myFunc(x) {
+    return x;
+  },
+  (a, b) => a * b,
+]);
